9563dababa6fd4f08c2f38a824482f7d7eb584eb
Oliver Gerlich <oliver.gerlich@gmx.de>
2018-07-18 23:12:35 +0200

    move debouncing logic to separate Debouncer class, and use it for list view
    While the normal GUI is re-rendered at most every 100 msec, the list is
    re-rendered at most every second (since it is very expensive to render).

--- webroot/js/lib.js
+++ webroot/js/lib.js
@@ -23,3 +23,31 @@ export function FromNowAgo(timeString) {
 	time /= 24;
 	return Math.round(time) + ' d';
 }
+
+
+/// Suppresses multiple rapid calls to a function, and instead calls the target function when a timeout has elapsed.
+export class Debouncer {
+	constructor (timeoutMsec, description) {
+		this.timeoutMsec = timeoutMsec;
+		this.desc = description;
+
+		this.timer = null;
+		this.numCallsSkipped = 0;
+	}
+
+	run (actualFunction) {
+		if (this.timer == null) {
+			actualFunction();
+			this.timer = window.setTimeout(() => {
+				this.timer = null;
+				if (this.numCallsSkipped > 0) {
+					console.log("skipped " + this.numCallsSkipped + " " + this.desc + " calls; running " + this.desc + " now");
+					this.numCallsSkipped = 0;
+					this.run(actualFunction);
+				}
+			}, this.timeoutMsec);
+		} else {
+			this.numCallsSkipped++;
+		}
+	}
+};
