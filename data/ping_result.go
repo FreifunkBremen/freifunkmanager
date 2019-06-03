9432190a0e7155b77a959acf2cc774d18eeb863e
Martin/Geno <geno+dev@fireorbit.de>
2019-06-03 15:38:46 +0200

    Add backend pinger

---
+++ data/ping_result.go
@@ -0,0 +1,6 @@
+package data
+
+type PingResult struct {
+    True  []string `json:"true"`
+    False []string `json:"false"`
+}
--- websocket/send.go
+++ websocket/send.go
@@ -6,6 +6,7 @@ import (
 	yanicRuntime "github.com/FreifunkBremen/yanic/runtime"
 
 	"github.com/FreifunkBremen/freifunkmanager/runtime"
+	"github.com/FreifunkBremen/freifunkmanager/data"
 )
 
 func (ws *WebsocketServer) SendNode(node *runtime.Node) {
@@ -15,9 +16,9 @@ func (ws *WebsocketServer) SendNode(node *runtime.Node) {
 func (ws *WebsocketServer) SendStats(data *yanicRuntime.GlobalStats) {
 	ws.ws.SendAll(&wsLib.Message{Subject: MessageTypeStats, Body: data})
 }
-func (ws *WebsocketServer) SendPing(data interface{}) {
+func (ws *WebsocketServer) SendPing(pResult *data.PingResult) {
 	var sessions []*Session
-	msg := &wsLib.Message{Subject: MessageTypePing, Body: data}
+	msg := &wsLib.Message{Subject: MessageTypePing, Body: pResult}
 
 	ws.db.Find(&sessions)
 	for _, session := range sessions {
